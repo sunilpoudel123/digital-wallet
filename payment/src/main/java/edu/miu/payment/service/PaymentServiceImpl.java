38a4997d49700a7b78cdb9aa2c6b45b28c8cad64
Sunil Poudel <sunil.poudel@miu.edu>
2025-06-11 14:28:21 -0500

    payment api : wip

--- payment/src/main/java/edu/miu/payment/service/PaymentServiceImpl.java
+++ payment/src/main/java/edu/miu/payment/service/PaymentServiceImpl.java
@@ -2,7 +2,6 @@ package edu.miu.payment.service;
 
 import edu.miu.payment.dto.MobileTopupRequest;
 import edu.miu.payment.dto.PaymentMethod;
-import edu.miu.payment.dto.PaymentRequest;
 import edu.miu.payment.dto.PaymentStatus;
 import edu.miu.payment.entity.Transaction;
 import edu.miu.payment.entity.TransactionType;
@@ -11,15 +10,15 @@ import edu.miu.payment.repository.PaymentTransactionRepository;
 import edu.miu.payment.repository.WalletRepository;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
-import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class PaymentServiceImpl implements PaymentService {
 
     private final PaymentTransactionRepository paymentTransactionRepository;
@@ -32,7 +31,7 @@ public class PaymentServiceImpl implements PaymentService {
     }
 
     @Override
-    @CircuitBreaker(name= "payment-service", fallbackMethod = "fallback")
+    @CircuitBreaker(name = "payment-service", fallbackMethod = "fallback")
     @Retry(name = "payment-service")
     public Transaction initiateMobileTopup(MobileTopupRequest mobileTopupRequest) {
         System.out.println("validation of payment request: ");
@@ -54,12 +53,13 @@ public class PaymentServiceImpl implements PaymentService {
         return paymentTransactionRepository.save(payment);
     }
 
-    public String fallback(Throwable t){
+    public String fallback(Throwable t) {
         return "fall back due to" + t.getMessage();
     }
 
     @Override
     public Transaction getPaymentStatus(Long paymentId) {
+        log.debug("Payment Id: {}", paymentId);
         return paymentTransactionRepository.findById(paymentId).orElseThrow(() -> new IllegalArgumentException("Payment not found"));
     }
 }
