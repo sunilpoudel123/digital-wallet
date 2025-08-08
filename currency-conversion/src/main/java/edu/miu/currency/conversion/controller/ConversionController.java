f0a87bb46014f03ebc9d35c486ab9e98e96fc7de
Sunil Poudel <sunil.poudel@miu.edu>
2025-08-08 10:48:09 -0500

    minor name correction

--- currency-conversion/src/main/java/edu/miu/currency/conversion/controller/ConversionController.java
+++ currency-conversion/src/main/java/edu/miu/currency/conversion/controller/ConversionController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/convert")
-public class ConversationController {
+public class ConversionController {
 
     @Autowired
     private ConversionService conversionService;
